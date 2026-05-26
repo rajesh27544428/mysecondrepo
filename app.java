adeae44839c571ae899a35755e5a69c4a825f63c
root <root@ip-172-31-34-107.eu-north-1.compute.internal>
2026-05-26 08:01:35 +0000

    code added successfully

---
+++ app.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    // The main method is the entry point of the program
+    public static void main(String[] args) {
+        // Output text to the console
+        System.out.println("Hello, World!");
+    }
+}
